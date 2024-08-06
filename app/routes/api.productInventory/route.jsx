034e18449c1250c901197bc6829cd5ae4a7631a2
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2024-08-06 14:49:57 +0530

    db changes sqlite to postgres

--- app/routes/api.productInventory/route.jsx
+++ app/routes/api.productInventory/route.jsx
@@ -2,9 +2,11 @@ import { json } from "@remix-run/node";
 import db from "../../db.server";
 import { cors } from "remix-utils/cors";
 
-export async function action({ request }) {
+export async function loader({ request }) {
     const body = await request.json(); 
-    const { shop, productId } = body;
+    //const { shop, productId } = body;
+    let shop = searchParams.get("shop");
+    let productId = searchParams.get("productId");
   
 
   const token = await db.session.findFirst({
@@ -31,8 +33,45 @@ export async function action({ request }) {
    
     let data=await response.json();
 
+
+    const store = await db.userConnection.findFirst({
+      where: { shop },
+    });
+    const myHeadersqty = new Headers();
+    myHeaders.append("Authorization", "Bearer " + store.token);
+    const requestOptionsqty = {
+      method: "GET",
+      headers: myHeadersqty,
+      redirect: "follow",
+    };
+  
+    try {
+      const response = await fetch(
+        "https://main.dev.saasintegrator.online/api/v1/click_and_collect/config-form",
+        requestOptionsqty,
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      let data = await response.json();
+      let quantity = 0;
+      data.config_form.map((item, index) => {
+        if (item?.saved_values?.shopify_minimum_pickup_stock_quantity_check=='yes' && item?.saved_values?.shopify_minimum_pickup_stock_quantity_value!='') {
+          quantity = item.saved_values?.shopify_minimum_pickup_stock_quantity_value;
+        }
+      });
+      //return await cors(request, json({ quantity: quantity }));
+      }
+      catch(error) {
+          // return await cors(request, json({ quantity: 0 }));
+          let quantity = 0;
+      }
+      const newdata = [...data,{quantity}]
     return await cors(request, json({ "data": data}));
   
+    
+
+
     
  // } catch (error) {console.error("Error fetching inventory locations:", error);return await cors(request, json({ "error":"500" }));}
   // console.log(token);
