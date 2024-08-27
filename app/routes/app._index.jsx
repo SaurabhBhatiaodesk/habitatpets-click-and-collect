21dead2522c1be21493d4551cf50ee46551896bd
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2024-08-27 13:22:29 +0530

    kilometer added

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -44,12 +44,16 @@ export const loader = async ({ request }) => {
     }
 
     // Fetch user connection information
-    const store = await db.userConnection.findFirst({
+    let store = await db.userConnection.findFirst({
       where: { shop },
     });
 
     if (!store) {
-      throw new Error("User connection information not found for the shop.");
+      getAuthToken();
+       store = await db.userConnection.findFirst({
+        where: { shop },
+      });
+      //throw new Error("User connection information not found for the shop.");
     }
 
     console.log("Store Data:", store);
@@ -133,6 +137,150 @@ export const loader = async ({ request }) => {
     throw new Error("An error occurred while loading the data.");
   }
 };
+async function getAuthToken(session) {
+  console.log("getAuthToken ------------------->")
+  try {
+    console.log(session,'session');
+    var myHeaders2 = new Headers();
+    myHeaders2.append("X-Shopify-Access-Token", session.accessToken);
+
+    const requestOptions2 = {
+      method: "GET",
+      headers: myHeaders2,
+      redirect: "follow"
+    };
+
+  await fetch('https://'+ session.shop + "/admin/api/2024-04/shop.json", requestOptions2)
+    .then((response) => response.json())
+    .then(async(result) => {
+      console.log(result, 'result');
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      
+        
+
+      const requestOpt = {
+        method: "GET",
+        redirect: "follow"
+      };
+      
+      let plugin=await fetch("https://main.dev.saasintegrator.online/api/v1/platforms", requestOpt);
+      console.log(plugin,":::plugin");
+      let plugin_data=await plugin.json();
+      console.log(plugin_data,":::plugin_data");
+      let filter_data = plugin_data.platforms.filter((pd) => pd.name === "o360-retail-express" || pd.name === "o360-shopify"); 
+      console.log(filter_data,":::filter_data");
+       const plugin_ids=[];
+       filter_data.map((fd)=>{
+        plugin_ids.push(fd.id);
+       });
+       const raw =  JSON.stringify({
+        ...session,
+        "state": session?.state.trim() !== "" ? session?.state : null,
+        'email' : result.shop?.email,
+        "plugin_ids":plugin_ids
+      });
+         console.log('testing ', raw);
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow"
+        };
+        const email = result.shop?.email;
+      await fetch("https://main.dev.saasintegrator.online/api/v1/user-connection", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+     
+          console.log('testing result', result);
+
+            const data = {
+              shop:session.shop,
+              ...result.data.connection,
+              token: result?.data?.token,
+              email: email
+          };
+          console.log('testings ', data);
+        
+        createOrUpdateUserConnection(data)
+            .then(result => {
+                console.log("User connection created or updated:", result);
+            });
+        });
+
+    });
+  }
+  catch(error){
+    console.log('error',error);
+  }
+
+}
+
+
+async function createOrUpdateUserConnection(data) {
+// console.log('createOrUpdateUserConnection ',data)
+const existingUserConnection = await prisma.userConnection.findFirst({
+  where: {
+      shop: data.shop,
+  },
+});
+
+if (existingUserConnection) {
+  if(data?.token){
+  const updatedUserConnection = await prisma.userConnection.update({
+      where: {
+          shop: data.shop,
+      },
+      data: {
+          // connection_id: data.id,
+          // uid: data.uid,
+          // user_id: data.user_id,
+          // plan_id: data.plan_id,
+          // custom_name: data.custom_name,
+          // custom_note: data.custom_note,
+          // sync_type: data.sync_type,
+          // configured: data.configured,
+          // is_sync_enabled: data.is_sync_enabled,
+          // is_plugins_connected: data.is_plugins_connected,
+          // config: data.config,
+          // created_at: data.created_at,
+          // updated_at: data.updated_at,
+          // status: data.status,
+          // active_subscription_id: data.active_subscription_id,
+          token: data?.token
+      },
+  });
+  return updatedUserConnection;
+  }
+  else{
+    return existingUserConnection;
+  }
+} else {
+    const newUserConnection = await prisma.userConnection.create({
+        data: {
+            connection_id: data.id,
+            shop: data.shop,
+            uid: data.uid,
+            user_id: data.user_id,
+            plan_id: data.plan_id,
+            custom_name: data.custom_name,
+            custom_note: data.custom_note,
+            sync_type: data.sync_type,
+            configured: data.configured,
+            is_sync_enabled: data.is_sync_enabled,
+            is_plugins_connected: data.is_plugins_connected,
+            config: data.config,
+            created_at: data.created_at,
+            updated_at: data.updated_at,
+            status: data.status,
+            active_subscription_id: data.active_subscription_id,
+            token: data?.token,
+            email: data.email,
+        },
+    });
+    return newUserConnection;
+}
+}
 
 
 export default function configPage() {
--- app/shopify.server.js
+++ app/shopify.server.js
@@ -134,6 +134,7 @@ async function getAuthToken(session) {
 
 
 async function createOrUpdateUserConnection(data) {
+// console.log('createOrUpdateUserConnection ',data)
 const existingUserConnection = await prisma.userConnection.findFirst({
   where: {
       shop: data.shop,
