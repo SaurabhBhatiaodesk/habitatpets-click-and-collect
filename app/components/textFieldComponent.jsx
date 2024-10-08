64d2a6d0b26c8b91967b35ec351b593e9445b352
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2024-10-08 13:55:50 +0530

    fix

--- app/components/textFieldComponent.jsx
+++ app/components/textFieldComponent.jsx
@@ -20,11 +20,7 @@ const TextFieldComponent = ({ field, inputValues, handleconfigChange, mango, err
     // Check if the field is cloneable (multiple values) or not
     if (field.is_cloneable) {
       // Initialize fields for multiple values
-      if (value && typeof value === 'string') {
-        setFields(initialValues.map(value => ({ value: value.trim() })));
-    }
-    
-      
+      setFields(initialValues.map(value => ({ value: value.trim() })));
     } else {
       // Initialize single field value
       setSingleField(initialValues[0] || ''); // Use the first value if multiple values are returned
